aa02ba593c035415b461817c12e85502225ff72d
khoaanh4920 <khoaanh4920@gmail.com>
2026-04-11 19:36:44 +0700

    fix build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "kvault-backend",
       cwd: "./apps/backend",
-      script: "dist/main.js",
+      script: "dist/src/main.js",
       instances: 1,
       autorestart: true,
       watch: false,
